0647f6b53c76abca2a9e6ee1e5c5e2333f8aa390
Wiktor <wpiela9@gmail.com>
2023-03-02 07:29:55 +0100

    add answer wiew and template

--- forumapp/views.py
+++ forumapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question, Answer
 from .forms import QuestionForm, AnswerForm
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 def home(request):
     wall = Question.objects.order_by("-create_date")
@@ -40,4 +42,37 @@ def compose_new_question(request):
         
 def add_answer(request, questionID):
     question = get_object_or_404(Question, pk=questionID)
-    return render(request, "add_answer.html", {"question":question})
+    if request.method == "GET":
+        return render(request, "add_answer.html", {"question":question})
+    else:
+        form = AnswerForm(request.POST, request.FILES)
+        author_email = request.user.email
+        if form.is_valid():
+            new_answer = form.save(commit=False)
+            new_answer.user = request.user
+            new_answer.question = question
+            new_answer.save()
+
+            if question.user != request.user:
+                email = EmailMessage(
+                    f"Your question '{question.title}' has been asnwered by {request.user.username}",
+                    f"""Congratulations, <br>
+                    {request.user.username} has just added the answer to your question!
+                    Content of the response:
+                    {new_answer.desc} <br>
+                    Best regards, Cars Forum""",
+                    settings.DEFAULT_FROM_EMAIL,
+                    [author_email])
+                email.content_subtype = "html"
+                email.send()
+                return redirect("forumapp:question_details", questionID=question.id)
+            else:
+                return redirect("forumapp:question_details", questionID=question.id)
+    
+        else:
+            message = "Something went wrong, please try again!"
+            return render(request, "add_answer.html", {"question":question, "message":message})
+                
+
+
+
